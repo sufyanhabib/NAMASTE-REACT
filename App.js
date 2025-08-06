7e12b728d01fd8267e8d27ef741c895f2328e766
sufyanhabib <sufyanhabib80@gmail.com>
2025-08-06 20:48:07 +0530

    JSX makes it easier to write and add HTML in React

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
-const heading = React.createElement(
-  "div",
-  { className: "heading" },
-  React.createElement("h1", null, "Welcome to My App"),
-  React.createElement("p", null, "This is a simple React application."),
-  React.createElement(
-    "button",
-    { onClick: () => alert("Button Clicked!") },
-    "Click Me"
-  ),
-  React.createElement("img", {
-    src: "https://via.placeholder.com/150",
-    alt: "Placeholder Image",
-  }),
-  React.createElement("footer", null, "© 2023 My App"),
-  React.createElement(
-    "a",
-    { href: "https://www.example.com", target: "_blank" },
-    "Visit Example.com"
-  )
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Importing React and ReactDOM to render the component
+// React is used to create components and ReactDOM is used to render them to the DOM
+
+// Creating a React element (a component) to render
+// Here, we are creating a simple heading element
+// This is a JSX syntax which will be transpiled to JavaScript
+
+const heading = <h1>Hello, World from React</h1>;
+
+const list = (
+  <ul>
+    <li>Apple</li>
+    <li>Banana</li>
+    <li>Orange</li>
+    <li>Mango</li>
+  </ul>
+);
+
+const x = 20;
+let text = "Hello, World!";
+if (x < 10) {
+  text = "Hello, React!";
+}
+const element2 = <h1>{text}</h1>;
+const element = (
+  <div>
+    {heading} {list} {10 + 20} {/* This will render the sum of 10 and 20 */}
+    {element2}
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(element);
